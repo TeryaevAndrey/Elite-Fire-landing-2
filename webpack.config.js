733866f972f0d43a0911b00739ebe27f6649b392
Andrey <teryaev.2004@mail.ru>
2023-02-20 18:45:22 +0300

    start-advantages-section

--- webpack.config.js
+++ webpack.config.js
@@ -4,37 +4,47 @@ const miniCssExtractPlugin = require("mini-css-extract-plugin");
 const copyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-    entry: {
-        main: path.resolve(__dirname, "./src/index.js")
-    },
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "[name].bundle.js",
-    },
-    module: {
-        rules: [
-            {test: /\.svg$/, use: "svg-inline-loader"},
-            {test: /\.s[ac]ss$/, use: ["style-loader", "css-loader", "sass-loader"]},
-            {test: /\.(js)$/, use: "babel-loader"},
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-              }
-        ]
-    },
-    plugins: [
-        new miniCssExtractPlugin(),
-        new copyWebpackPlugin({
-            patterns: [
-                {
-                    from: path.resolve(__dirname, "src/images"),
-                    to: "images"
-                },
-            ]
-        }),
-        new htmlWebpackPlugin({
-            template: path.resolve(__dirname, "./src/index.html"),
-            filename: "index.html"
-        })
-    ]
-}
+  entry: {
+    main: path.resolve(__dirname, "./src/index.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      { test: /\.(js)$/, use: "babel-loader" },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(ttf|eot|woff2?)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new copyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "./src/images"),
+          to: "dist/images"
+        },
+        {
+          from: path.resolve(__dirname, "./src/fonts"),
+          to: "dist/fonts"
+        },
+      ],
+    }),
+    new miniCssExtractPlugin(),
+    new htmlWebpackPlugin({
+      template: path.resolve(__dirname, "./src/index.html"),
+      filename: "index.html",
+    }),
+  ],
+};
