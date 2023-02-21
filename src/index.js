0119858b3389b05985b25de00f7ecca0d831f231
Andrey <teryaev.2004@mail.ru>
2023-02-21 14:00:29 +0300

    fix-scss-and-adaptive-before-sections

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import "./scss/style.scss";
+
+const burgerBtn = document.querySelector(".burger");
+const menu = document.querySelector(".menu");
+
+burgerBtn.addEventListener("click", () => {
+    menu.classList.toggle("active");
+});
