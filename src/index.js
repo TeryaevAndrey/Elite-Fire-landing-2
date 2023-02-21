03554b5ee15db3cc2b92997e822d080794683fd9
Andrey <teryaev.2004@mail.ru>
2023-02-21 22:10:05 +0300

    add-sections

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import Swiper, {Navigation} from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 import "./scss/style.scss";
 
 const burgerBtn = document.querySelector(".burger");
@@ -6,3 +10,13 @@ const menu = document.querySelector(".menu");
 burgerBtn.addEventListener("click", () => {
     menu.classList.toggle("active");
 });
+
+const slider = new Swiper(".swiper", {
+    loop: true, 
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+    modules: [Navigation]
+});
+
